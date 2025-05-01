36e8b00b3eb76a65b1a7a3780ab8162f74abf679
jserra7d5 <progamer5051@gmail.com>
2025-05-01 09:37:45 -0700

    add layer filtering

--- main.R
+++ main.R
@@ -8,24 +8,32 @@ library(magrittr)
 soil_polygons <- st_read("CSNM_Polygons_with_Data.geojson", quiet = FALSE) %>%
   st_transform(crs = 4326)
 
+orders <- unique(soil_polygons$taxorder)
+pal <- colorFactor("Set3", domain = orders)
+map <- leaflet() %>%
+  addTiles()
 
-print(st_bbox(soil_polygons))
 
-
-leaflet() %>%
-  addTiles() %>%
-  addPolygons(
-    data       = soil_polygons,
-    fillColor  = ~colorFactor("Set3", MUKEY)(MUKEY),
-    fillOpacity= 0.6,
-    color      = "white",
-    weight     = 1,
-    popup      = ~paste0(
-      "<b>Taxonomic Order:</b> ", taxorder, "<br>",
-      "<b>Taxonomic  sub-Order:</b> ", taxsuborder, "<br>",
-      "<b>MUKEY:</b> ", MUKEY, "<br>",
-      "<b>Component:</b> ", compname
+for(o in orders) {
+  map <- map %>%
+    addPolygons(
+      data       = subset(soil_polygons, taxorder == o),
+      fillColor  = pal(o),
+      fillOpacity= 0.6,
+      color      = "white",
+      weight     = 1,
+      group      = o,
+      popup      = ~paste0("<b>Order:</b> ", taxorder,
+                           "<br><b>Taxonomic  sub-Order:</b> ", taxsuborder,
+                           "<br><b>MUKEY:</b> ", MUKEY,
+                           "<br><b>Component:</b> ", compname)
     )
+}
+
+map %>%
+  addLayersControl(
+    overlayGroups = orders,
+    options       = layersControlOptions(collapsed = FALSE)
   )
 
 #run names(soil_polygons) to get the component terms
